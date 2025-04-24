1da4c791f22a03214953a3786bfa1bb9dc54f062
Kyle Mendell <kmendell@ofkm.us>
2025-04-24 17:41:05 -0500

    fix: remove badges on detail pages

---
+++ src/routes/containers/components/NameCell.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  interface Props {
+    id: string;
+    name: string;
+  }
+
+  let { id, name }: Props = $props();
+</script>
+
+<a href="/containers/{id}" class="font-medium hover:underline">
+  {name}
+</a>
