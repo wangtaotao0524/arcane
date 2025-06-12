bc156687410f1c38677b63752904e8385599aa07
Kyle Mendell <ksm@ofkm.us>
2025-06-12 12:46:00 -0500

    chore(build): restructure dockerfile

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,7 @@ WORKDIR /build
 COPY ./frontend/package*.json ./
 RUN npm ci
 COPY ./frontend ./
-RUN npm run build
+RUN BUILD_PATH=dist npm run build
 
 # Stage 2: Build Backend
 FROM golang:1.24-alpine AS backend-builder
--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -6,7 +6,7 @@ const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			pages: '../backend/frontend/dist',
+			pages: process.env.BUILD_PATH ?? '../backend/frontend/dist',
 			fallback: 'index.html'
 		}),
 		paths: {
