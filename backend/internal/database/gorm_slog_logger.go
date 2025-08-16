6ea6c77dc55faa629919ce626e1c6e5cf08ee07e
Kyle Mendell <ksm@ofkm.us>
2025-08-15 22:59:57 -0500

    feat: migrate logging to json

---
+++ backend/internal/database/gorm_slog_logger.go
@@ -0,0 +1,87 @@
+package database
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"time"
+
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+// slogGormLogger implements gorm/logger.Interface using Go's slog for JSON logs.
+type slogGormLogger struct {
+	level                     logger.LogLevel
+	slowThreshold             time.Duration
+	ignoreRecordNotFoundError bool
+}
+
+func newSlogGormLogger(level logger.LogLevel, slow time.Duration, ignoreNotFound bool) *slogGormLogger {
+	return &slogGormLogger{level: level, slowThreshold: slow, ignoreRecordNotFoundError: ignoreNotFound}
+}
+
+func (l *slogGormLogger) LogMode(level logger.LogLevel) logger.Interface {
+	nl := *l
+	nl.level = level
+	return &nl
+}
+
+func (l *slogGormLogger) Info(ctx context.Context, msg string, data ...interface{}) {
+	if l.level > logger.Info {
+		return
+	}
+	slog.InfoContext(ctx, msg, slog.Any("data", data))
+}
+
+func (l *slogGormLogger) Warn(ctx context.Context, msg string, data ...interface{}) {
+	if l.level > logger.Warn {
+		return
+	}
+	slog.WarnContext(ctx, msg, slog.Any("data", data))
+}
+
+func (l *slogGormLogger) Error(ctx context.Context, msg string, data ...interface{}) {
+	if l.level > logger.Error {
+		return
+	}
+	slog.ErrorContext(ctx, msg, slog.Any("data", data))
+}
+
+func (l *slogGormLogger) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
+	if l.level == logger.Silent {
+		return
+	}
+
+	elapsed := time.Since(begin)
+	sql, rows := fc()
+
+	if err != nil && l.level <= logger.Error {
+		if !(l.ignoreRecordNotFoundError && errors.Is(err, gorm.ErrRecordNotFound)) {
+			slog.ErrorContext(ctx, "gorm.trace",
+				slog.Duration("elapsed", elapsed),
+				slog.Int64("rows", rows),
+				slog.String("sql", sql),
+				slog.String("error", err.Error()),
+			)
+		}
+		return
+	}
+
+	if l.slowThreshold > 0 && elapsed > l.slowThreshold && l.level <= logger.Warn {
+		slog.WarnContext(ctx, "gorm.slow_query",
+			slog.Duration("elapsed", elapsed),
+			slog.Int64("rows", rows),
+			slog.String("sql", sql),
+		)
+		return
+	}
+
+	if l.level <= logger.Info {
+		slog.InfoContext(ctx, "gorm.query",
+			slog.Duration("elapsed", elapsed),
+			slog.Int64("rows", rows),
+			slog.String("sql", sql),
+		)
+	}
+}
