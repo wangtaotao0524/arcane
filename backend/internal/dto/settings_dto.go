a89581e2a3229c29bc6cd9628edbc3191ba0795c
Kyle Mendell <ksm@ofkm.us>
2025-10-27 17:22:49 -0500

    chore: run formatter

--- backend/internal/dto/settings_dto.go
+++ backend/internal/dto/settings_dto.go
@@ -12,23 +12,23 @@ type SettingDto struct {
 }
 
 type UpdateSettingsDto struct {
-	ProjectsDirectory            *string `json:"projectsDirectory,omitempty"`
-	DiskUsagePath                *string `json:"diskUsagePath,omitempty"`
-	AutoUpdate                   *string `json:"autoUpdate,omitempty"`
-	AutoUpdateInterval           *string `json:"autoUpdateInterval,omitempty"`
-	PollingEnabled               *string `json:"pollingEnabled,omitempty"`
-	PollingInterval              *string `json:"pollingInterval,omitempty"`
-	PruneMode                    *string `json:"dockerPruneMode,omitempty" binding:"omitempty,oneof=all dangling"`
-	BaseServerURL                *string `json:"baseServerUrl,omitempty"`
-	EnableGravatar               *string `json:"enableGravatar,omitempty"`
-	DefaultShell                 *string `json:"defaultShell,omitempty"`
-	DockerHost                   *string `json:"dockerHost,omitempty"`
-	AccentColor                  *string `json:"accentColor,omitempty"`
-	AuthLocalEnabled             *string `json:"authLocalEnabled,omitempty"`
-	AuthOidcEnabled              *string `json:"authOidcEnabled,omitempty"`
-	AuthSessionTimeout           *string `json:"authSessionTimeout,omitempty"`
-	AuthPasswordPolicy           *string `json:"authPasswordPolicy,omitempty"`
-	AuthOidcConfig               *string `json:"authOidcConfig,omitempty"`
+	ProjectsDirectory          *string `json:"projectsDirectory,omitempty"`
+	DiskUsagePath              *string `json:"diskUsagePath,omitempty"`
+	AutoUpdate                 *string `json:"autoUpdate,omitempty"`
+	AutoUpdateInterval         *string `json:"autoUpdateInterval,omitempty"`
+	PollingEnabled             *string `json:"pollingEnabled,omitempty"`
+	PollingInterval            *string `json:"pollingInterval,omitempty"`
+	PruneMode                  *string `json:"dockerPruneMode,omitempty" binding:"omitempty,oneof=all dangling"`
+	BaseServerURL              *string `json:"baseServerUrl,omitempty"`
+	EnableGravatar             *string `json:"enableGravatar,omitempty"`
+	DefaultShell               *string `json:"defaultShell,omitempty"`
+	DockerHost                 *string `json:"dockerHost,omitempty"`
+	AccentColor                *string `json:"accentColor,omitempty"`
+	AuthLocalEnabled           *string `json:"authLocalEnabled,omitempty"`
+	AuthOidcEnabled            *string `json:"authOidcEnabled,omitempty"`
+	AuthSessionTimeout         *string `json:"authSessionTimeout,omitempty"`
+	AuthPasswordPolicy         *string `json:"authPasswordPolicy,omitempty"`
+	AuthOidcConfig             *string `json:"authOidcConfig,omitempty"`
 	OnboardingCompleted        *string `json:"onboardingCompleted,omitempty"`
 	OnboardingSteps            *string `json:"onboardingSteps,omitempty"`
 	MobileNavigationMode       *string `json:"mobileNavigationMode,omitempty"`
